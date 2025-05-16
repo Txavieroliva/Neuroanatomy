696456b1e4cf1337d17a2fb41bc1258dc15c5590
Jean Pierre Kerberos <MenteEnElArbol@keemail.me>
2025-05-16 00:14:20 -0300

    Actualización del UI con paneles laterales

---
+++ Assets/Script/ControlOfToggle.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class MultiUIToggler : MonoBehaviour
+{
+    [System.Serializable]
+    public class ToggleUI
+    {
+        public Toggle toggle;
+        public GameObject targetUI;
+    }
+
+    public List<ToggleUI> toggleUIList;
+
+    void Start()
+    {
+        foreach (var item in toggleUIList)
+        {
+            item.toggle.onValueChanged.AddListener((bool isOn) =>
+            {
+                item.targetUI.SetActive(isOn);
+            });
+
+            // Inicializar visibilidad
+            item.targetUI.SetActive(item.toggle.isOn);
+        }
+    }
+}
